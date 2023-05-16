0c14f306e629fb7a8b50dbd61ba66b164feb1a1f
Tommaso Amorosi <tomy.bawse@gmail.com>
2023-05-16 09:56:39 +0200

    Random commits

--- App.py
+++ App.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+@app.route("/", methods = ["GET", "POST"])
+def BMI_calculator():
+    bmi = 0
+    if request.method == "POST":
+        weight = float(request.form["weight"])
+        height = float(request.form["height"])
+        bmi = weight / (height ** 2)
+
+    return render_template("index.html", bmi = bmi)
+
+app.run(debug= True)
